2221ba2241a307531aa2bfcd89759bc44ff8a554
Jonathan Challinger <mr.challinger@gmail.com>
2017-11-10 15:17:24 -0800

    update framework

--- src/main.c
+++ src/main.c
@@ -15,18 +15,18 @@
  */
 
 #include <ch.h>
-#include <modules/param/param.h>
-
-PARAM_DEFINE_FLOAT32_PARAM_STATIC(param_a, "a", 4, 3, 5)
-PARAM_DEFINE_INT64_PARAM_STATIC(param_b, "b", 7, 7, 7)
-PARAM_DEFINE_INT32_PARAM_STATIC(param_c, "c", 7, 7, 7)
-PARAM_DEFINE_INT16_PARAM_STATIC(param_d, "d", 7, 7, 7)
-PARAM_DEFINE_INT8_PARAM_STATIC(param_e, "e", 7, 7, 7)
-PARAM_DEFINE_UINT32_PARAM_STATIC(param_f, "f", 7, 7, 7)
-PARAM_DEFINE_UINT16_PARAM_STATIC(param_g, "g", 7, 7, 7)
-PARAM_DEFINE_UINT8_PARAM_STATIC(param_h, "h", 7, 7, 7)
-PARAM_DEFINE_STRING_PARAM_STATIC(param_j, "j", "blah", 128)
-PARAM_DEFINE_BOOL_PARAM_STATIC(param_i, "i", true)
+// #include <modules/param/param.h>
+//
+// PARAM_DEFINE_FLOAT32_PARAM_STATIC(param_a, "a", 4, 3, 5)
+// PARAM_DEFINE_INT64_PARAM_STATIC(param_b, "b", 7, 7, 7)
+// PARAM_DEFINE_INT32_PARAM_STATIC(param_c, "c", 7, 7, 7)
+// PARAM_DEFINE_INT16_PARAM_STATIC(param_d, "d", 7, 7, 7)
+// PARAM_DEFINE_INT8_PARAM_STATIC(param_e, "e", 7, 7, 7)
+// PARAM_DEFINE_UINT32_PARAM_STATIC(param_f, "f", 7, 7, 7)
+// PARAM_DEFINE_UINT16_PARAM_STATIC(param_g, "g", 7, 7, 7)
+// PARAM_DEFINE_UINT8_PARAM_STATIC(param_h, "h", 7, 7, 7)
+// PARAM_DEFINE_STRING_PARAM_STATIC(param_j, "j", "blah", 128)
+// PARAM_DEFINE_BOOL_PARAM_STATIC(param_i, "i", true)
 
 int main(void) {
     chThdSleep(TIME_INFINITE);
