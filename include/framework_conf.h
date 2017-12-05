4ff46401a2c1587f5b55fcfb165786e28674c169
Siddharth Purohit <siddharthbharatpurohit@gmail.com>
2017-12-05 21:58:31 +0530

    config:update config files per updates to framework

---
+++ include/framework_conf.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define CH_CFG_USE_HEAP                     TRUE
+#define CH_USE_MALLOC_HEAP                  TRUE
+#define CH_DBG_SYSTEM_STATE_CHECK           TRUE
+#define CH_DBG_ENABLE_CHECKS                TRUE
+#define CH_DBG_ENABLE_ASSERTS               TRUE
+#define CH_DBG_ENABLE_STACK_CHECK           TRUE
+#define CH_DBG_FILL_THREADS                 TRUE
